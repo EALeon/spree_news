a6d124d13b94ec05eb1b1899267715a715e0eb98
«Evgeny <caldariset@gmail.com>
2015-12-18 17:40:51 +0400

    scope error fix

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -2,5 +2,6 @@ class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description
 
   scope :published, lambda { where(:published => true) }
-  scope :latest, order("created_at DESC").limit(3)
+  scope :latest, -> { order('created_at DESC').limit(3) }
+
 end
